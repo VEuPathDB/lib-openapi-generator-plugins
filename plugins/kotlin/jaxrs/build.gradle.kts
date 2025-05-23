ac2269607b64b7fa53f155917e25734387ef9f1b
Elizabeth Paige Harper <elizabeth.harper@foxcapades.io>
2025-05-23 09:38:53 -0400

    fix build

--- plugins/kotlin/jaxrs/build.gradle.kts
+++ plugins/kotlin/jaxrs/build.gradle.kts
@@ -1,7 +1,10 @@
 import org.jetbrains.kotlin.gradle.dsl.JvmTarget
 
+version = "1.0.0-SNAPSHOT"
+
 plugins {
   kotlin("jvm") version "2.1.21"
+  `maven-publish`
 }
 
 repositories {
@@ -19,5 +22,42 @@ java {
 kotlin {
   compilerOptions {
     jvmTarget = JvmTarget.JVM_11
+  }
+}
+
+publishing {
+  repositories {
+    maven {
+      name = "GitHub"
+      url = uri("https://maven.pkg.github.com/VEuPathDB/lib-openapi-generator-plugins")
+      credentials {
+        username = project.findProperty("gpr.user") as String? ?: System.getenv("USERNAME")
+        password = project.findProperty("gpr.key") as String? ?: System.getenv("TOKEN")
+      }
+    }
+
+    publications {
+      create<MavenPublication>(name) {
+        from(components["java"])
+
+        artifactId = "oas-${name}"
+
+        pom {
+          name = "oas-${name}"
+          url = "https://github.com/VEuPathDB/lib-openapi-generator-plugins"
+
+          licenses { license { name = "Apache-2.0" } }
+
+          developers {
+            developer {
+              id = "epharper"
+              name = "Elizabeth Paige Harper"
+              email = "elizabeth.harper@foxcapades.io"
+              url = "https://github.com/foxcapades"
+            }
+          }
+        }
+      }
+    }
   }
 }
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,8 +1,7 @@
 rootProject.name = "openapi-jaxrs-kt"
 
-
-include(":plugins:kotlin:jaxrs")
-project(":plugins:kotlin:jaxrs").apply {
+include(":jaxrs-kt")
+project(":jaxrs-kt").apply {
   name = "jaxrs-kt"
   projectDir = file("plugins/kotlin/jaxrs")
 }
